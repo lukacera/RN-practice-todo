e01992b4e18a2c25cc2c508a79c56c4fb5348486
lukacera <lukaceranic38@gmail.com>
2024-05-19 18:05:55 +0200

    Insert all components in index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,23 @@
 import HeaderLogo from "@/components/HeaderLogo";
-import { View, Text, SafeAreaView } from "react-native"
+import TodoInput from "@/components/TodoInput";
+import { View, Text, SafeAreaView, TextInput } from "react-native";
 
-const indexPage = () => {
+const IndexPage = () => {
+
   return (
     <SafeAreaView className="h-screen bg-primary">
       <HeaderLogo />
+      {/* Main part, all of todos and input for it */}
+      <TodoInput />
+      {/* Todos list */}
+
+      <View className="flex items-center justify-center mt-10">
+        <Text>
+          My todos
+        </Text>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-
-export default indexPage;
+export default IndexPage;
